f3f6663c25ca3e22a1bc3213e0cb99738e96e4b9
Deborah <debbieadeoye1@gmail.com>
2022-04-28 16:58:43 +0100

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import { dark } from "./styles/Themes"
+
+
 function App() {
   return (
     <>
     <GlobalStyles />
       
-      <main className="App">
-        App File
-      </main>
+      <ThemeProvider theme={dark} >
+        <main className="App">
+          App File
+        </main>
+      </ThemeProvider>
     </>
   );
 }
