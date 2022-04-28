497ccba1460fdeab970a20f26600ebccb93deea3
Deborah <debbieadeoye1@gmail.com>
2022-04-28 20:03:41 +0100

    logo

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import CoverVideo from '../components/CoverVideo';
+import Logo from '../components/Logo';
 
 
 
@@ -14,7 +15,7 @@ const Home = () => {
   return (
 		<Section>
 			<CoverVideo />
-			<h1>Logo</h1>
+			<Logo />
 			<h1>NavBar</h1>
 
 		</Section>
